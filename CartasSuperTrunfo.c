1acb1c3c5224dd96b07387ce8db953217b829d89
Matheus <152727396+MatheusMarinhoLopes@users.noreply.github.com>
2025-10-24 00:36:25 +0000

    Declarei Variáveis da carta 1 e da carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,25 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  // declarando variáveis da carta 1
+
+        char estado1;
+        char codigo1[4];
+        char cidade1[50];
+        int populacao1;
+        float area1;
+        float pib1;
+        int pontosTuristicos1;
+
+  // declarando variavéis da carta 2
+
+        char estado2;
+        char codigo2[4];
+        char cidade2[50];
+        int populacao2;
+        float area2;
+        float pib2;
+        int pontosTuristicos2;
 
   // Área para entrada de dados
 
